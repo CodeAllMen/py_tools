ae9c9d11ec69781c6a0c2259d99fce0ef08e9ee4
yy <1023767856@qq.com>
2019-04-24 16:06:32 +0800

    change host

--- config/settings.py
+++ config/settings.py
@@ -2,3 +2,4 @@
 author songjie
 """
 PORT = 8077
+HOST = "0.0.0.0"
--- main.py
+++ main.py
@@ -6,4 +6,4 @@ from app import create_app
 app = create_app()
 
 if __name__ == "__main__":
-    app.run(debug=app.config['DEBUG'], port=app.config['PORT'], threaded=True)
+    app.run(debug=app.config['DEBUG'], host=app.config['HOST'], port=app.config['PORT'], threaded=True)
