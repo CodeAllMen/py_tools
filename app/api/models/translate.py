d92d3c5670231190ca2a3238a454aee3630ce202
yy <1023767856@qq.com>
2019-08-20 14:59:26 +0800

    ok

--- app/api/models/translate.py
+++ app/api/models/translate.py
@@ -1,10 +1,28 @@
 """
 author songjie
 """
+import html
+import re
+import sys
+import urllib.parse
+import urllib.request
+from html.parser import HTMLParser
+
 from googletrans import Translator
 
 
 class Translate(object):
+    agent = {'User-Agent':
+                 "Mozilla/4.0 (\
+                 compatible;\
+                 MSIE 6.0;\
+                 Windows NT 5.1;\
+                 SV1;\
+                 .NET CLR 1.1.4322;\
+                 .NET CLR 2.0.50727;\
+                 .NET CLR 3.0.04506.30\
+                 )"}
+
     def __init__(self):
         self.translator = Translator(service_urls=['translate.google.cn'])
 
@@ -17,3 +35,47 @@ class Translate(object):
         """
         data = self.translator.translate(content, src=origin_language, dest=aim_language).text
         return data
+
+    @staticmethod
+    def unescape(text):
+        if sys.version_info[0] < 3:
+            parser = HTMLParser.HTMLParser()
+        else:
+            parser = html.parser.HTMLParser()
+        return parser.unescape(text)
+
+    def start_translate(self, to_translate, to_language="auto", from_language="auto"):
+        # type: (object, object, object) -> object
+        # type: (object, object, object) -> object
+        """Returns the translation using google translate
+        you must shortcut the language you define
+        (French = fr, English = en, Spanish = es, etc...)
+        if not defined it will detect it or use english by default
+
+        Example:
+        print(translate("salut tu vas bien?", "en"))
+        hello you alright?
+        """
+        base_link = "http://translate.google.com/m?hl=%s&sl=%s&q=%s"
+        if sys.version_info[0] < 3:
+            to_translate = urllib.quote_plus(to_translate)
+            link = base_link % (to_language, from_language, to_translate)
+            request = urllib.Request(link, headers=self.agent)
+            raw_data = urllib.urlopen(request).read()
+        else:
+            to_translate = urllib.parse.quote(to_translate)
+            link = base_link % (to_language, from_language, to_translate)
+            request = urllib.request.Request(link, headers=self.agent)
+            raw_data = urllib.request.urlopen(request).read()
+        data = raw_data.decode("utf-8")
+        expr = r'class="t0">(.*?)<'
+        re_result = re.findall(expr, data)
+        if len(re_result) == 0:
+            result = ""
+        else:
+            result = html.unescape(re_result[0])
+        return result
+
+    def translate_com(self, to_translate, from_lang, to_lang):
+        result = self.start_translate(to_translate, to_lang, from_lang)
+        return result
--- app/api/translate.py
+++ app/api/translate.py
@@ -24,3 +24,20 @@ def translate():
         response_data["code"] = 1
         response_data["status"] = 500
     return Response(json.dumps(response_data), mimetype="application/json;charset=utf-8")
+
+
+@api.route("/translate_com")
+def translate_com():
+    response_data = dict()
+    try:
+        content = request.values.get("content")
+        src = request.values.get("src")
+        dest = request.values.get("dest")
+        response_data["data"] = Translate().translate_com(content, src, dest)
+        response_data["code"] = 0
+        response_data["status"] = 200
+    except Exception as e:
+        response_data["data"] = str(e)
+        response_data["code"] = 1
+        response_data["status"] = 500
+    return Response(json.dumps(response_data), mimetype="application/json;charset=utf-8")
